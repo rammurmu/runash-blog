71b6d63fbef5a2014652d852c6d156f643825980
Ram Murmu <runashInc@outlook.com>
2022-10-22 14:54:13 +0530

    Create _app.js

---
+++ _app.js
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+import Layout from '../components/Layout'
+import { SessionProvider } from "next-auth/react"
+
+function MyApp({ Component, pageProps, session }) {
+  return (
+    <SessionProvider session={session}>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    </SessionProvider>
+  )
+}
+
+export default MyApp
